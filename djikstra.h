9ef6603d59d3ad2b6a46d6ac26d98f7f5906cbd5
Hippovercken <149239166+Hippovercken@users.noreply.github.com>
2024-12-06 16:51:36 +0100

    djikstra.h

--- djikstra.h
+++ djikstra.h
@@ -6,10 +6,10 @@
 typedef struct {
     int* distances;
     int* predecesseurs;
-} DijkstraResultat;
+} Dijkstra;
 
-DijkstraResultat* dijkstra(Graphe* graphe, int sommetDepart);
-void afficherResultatsDijkstra(DijkstraResultat* resultat, int sommetArrivee, Graphe* graphe);
-void libererResultatsDijkstra(DijkstraResultat* resultat, Graphe* graphe);
+Dijkstra* dijkstra(Graphe* graphe, int sommetDepart);
+void afficherResultatsDijkstra(Dijkstra* resultat, int sommetArrivee, Graphe* graphe);
+void libererResultatsDijkstra(Dijkstra* resultat, Graphe* graphe);
 
 #endif //PROJET_INGE2_DIJKSTRA_H
